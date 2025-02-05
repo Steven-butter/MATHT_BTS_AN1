09a44e69d44911575f2f053a9d7f6d646cff5c86
Steven-butter <guillaume.steven@lycee-saintdenis.com>
2025-02-05 22:30:23 +0100

    Fonction fonctionnel cryptage

--- Python/Code Pin/test.py
+++ Python/Code Pin/test.py
@@ -1,13 +1,17 @@
 chiffre = [str(i) for i in range(10)]
 lettres = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+id_lettrefin = 0
 
-code = str(1234)
+code = str(1)
 def crypter(code):
     for i in range(len(code)):
         id_c = chiffre.index(code[i])
         id_l = id_c + 2
         print("id_c : ", id_c)
         print("id_l : ", id_l)
+        #lettrefin = str(id_l).index(lettres)
+        lettrefin = lettres[id_l - 1]
+        print("Lettre de fin : ", lettrefin)
         
 
 crypter(code)
