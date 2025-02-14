2f3fd9f98673a27e18cfaaa0e1fae4059fe10f53
Steven-butter <guillaume.steven@lycee-saintdenis.com>
2025-02-14 11:38:42 +0100

    fonction entrer_note ok

--- Python/Moyenne/moyenne.py
+++ Python/Moyenne/moyenne.py
@@ -4,16 +4,17 @@ matiere = [("Math", 3), ("SISR", 12), ("Cyber" , 14)]
 #notes = []
 
 ### i pour séléctionner 1 par 1 les matières
-### matiere : liste les matiere /// notes : liste de note
+### matiere : liste les matiere /// note : liste de note
 ### 
 
 def entrer_notes(matiere):
     note = []
-    for i in range (0,len(matiere)):
-        #notes.append(input(f'Saisir la note de {matiere[i][0]} : '))
-        note = input(f'Saisir la note de {matiere[i][0]} : ')
-        return note
+    for i in range (len(matiere)):
+        note.append(input(f'Saisir la note de {matiere[i][0]} : '))
+    print(note)
+    return note
+    
 
 
-entrer_notes()
-print(entrer_notes())
+entrer_notes(matiere)
+
