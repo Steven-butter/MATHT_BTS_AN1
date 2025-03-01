c1b9980e3ac57070efcd7bb119fdfe71cc83f8a2
Steven-butter <guillaume.steven@lycee-saintdenis.com>
2025-03-01 12:56:42 +0100

    a1 et a2

--- Python/Moyenne/moyenne.py
+++ Python/Moyenne/moyenne.py
@@ -4,7 +4,7 @@
 
 
 ### matière coef 
-matiere = [("Math", 3), ("SISR", 12), ("Cyber" , 14)]
+matiere = [("Math", 3), ("LV1", 2), ("LV2" , 1)]
 ### Boucle
 continuer = True
 
@@ -12,7 +12,23 @@ continuer = True
 ###             Fonctions                ###
 ############################################
 
+############################################
+### A1 : Calcul points bonus
 
+def calcul_bonus():
+    print("Entrer les points bonus")
+    bonus = int(input("Points bonus : "))
+    if bonus < 10:
+        bonus = 0
+        return bonus
+    else:
+        bonus = bonus - 10
+        bonus = bonus * 2
+        print(f'Vous avez {bonus} points bonus')
+        return bonus
+
+
+### A2 : Entrer les notes
 ### i pour séléctionner 1 par 1 les matières
 ### matiere : liste les matiere /// note : liste de note
 ### sort une liste de note
@@ -21,10 +37,7 @@ def entrer_notes(matiere):
     note = []
     for i in range (len(matiere)):
         note.append(input(f'Saisir la note de {matiere[i][0]} : '))
-    #print(note)
     return note
-
-test = entrer_notes(matiere)
 
 #############################################
 ###         Programme Principale          ###
@@ -45,7 +58,6 @@ while continuer:
     ########## C1 Entrer les notes ############# OK
 
     if choix == "1":
-        entrer_notes(matiere)
         notes = entrer_notes(matiere)
         for i in range (len(matiere)):
             print(f'Voici le tableau de note : {matiere[i][0], notes[i]}')
